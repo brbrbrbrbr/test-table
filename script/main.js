d00421731ce67908bf22fd32a074ccafd9cfc71a
brbrbrbrbr <lumos.colorum@gmail.com>
2018-06-13 17:26:09 +0300

    minor changes

--- script/main.js
+++ script/main.js
@@ -1,10 +1,18 @@
-function addEltmentToTable(){
-    var element = document.getElementById('test-content__test-table');
-    var addEltmentToTable = document.createElement("div");
-    addEltmentToTable.className = "test-content__square";
+
+
+function addColumnToTable(){
+    let rowNumber = 2;
+    let colNumber = 1;
+    let i = 0;
+    let j = 0;
+    let element = document.getElementById('test-content__test-table');
+    let addEltmentToTable = document.createElement("div");
+    addEltmentToTable.className = "col";
     element.appendChild(addEltmentToTable);
+    console.log('added col');
+
 }
 function removeEltmentFromTable(){
-    var element = document.getElementById('test-content__test-table');
+    let element = document.getElementById('test-content__test-table');
     element.removeChild(element);
 }
