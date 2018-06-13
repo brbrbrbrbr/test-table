61a9969e541fcb922e335eb72bdd154079921c9d
brbrbrbrbr <lumos.colorum@gmail.com>
2018-06-13 21:32:38 +0300

    adding columns

--- script/main.js
+++ script/main.js
@@ -1,10 +1,23 @@
-function addEltmentToTable(){
-    var element = document.getElementById('test-content__test-table');
-    var addEltmentToTable = document.createElement("div");
-    addEltmentToTable.className = "test-content__square";
+let i = 0;
+let j = 0;
+let rowNumber = 3;
+let colNumber = 3;
+function addColumnToTable(){
+    let element = document.getElementById('test-content__test-table');
+    let addEltmentToTable = document.createElement("div");
+    addEltmentToTable.className = "col";
     element.appendChild(addEltmentToTable);
+    console.log('added col');
+    let collElement = document.getElementById('test-content__test-table').lastChild;
+    for (i = 0; i < rowNumber; i++) {
+        let addEltmentTocolumn = document.createElement("div");
+        addEltmentTocolumn.className = "test-content__square";
+        collElement.appendChild(addEltmentTocolumn);
+        console.log('added div to col');
+    }
+    colNumber++;
 }
 function removeEltmentFromTable(){
-    var element = document.getElementById('test-content__test-table');
+    let element = document.getElementById('test-content__test-table');
     element.removeChild(element);
 }
