5610339ac2647f14cfb4fd728ed82bd4198862cc
brbrbrbrbr <lumos.colorum@gmail.com>
2018-06-14 15:52:03 +0300

    adding rows and columns

--- script/main.js
+++ script/main.js
@@ -2,7 +2,21 @@ let i = 0;
 let j = 0;
 let rowNumber = 3;
 let colNumber = 3;
+
 let element = document.getElementById('test-content__test-table');
+element.addEventListener( 'mouseover', function () {
+        let buttonVisibility = document.getElementsByClassName('test-content__red-button');
+        for (j = 0; j < 2; j++) {
+            buttonVisibility[j].style.opacity = '1';
+        }
+})
+element.addEventListener( 'mouseout', function () {
+    let buttonVisibility = document.getElementsByClassName('test-content__red-button');
+    for (j = 0; j < 2; j++) {
+        buttonVisibility[j].style.opacity = '0';
+    }
+})
+
 function addColumnToTable(){
     let addEltmentToTable = document.createElement("div");
     addEltmentToTable.className = "col";
