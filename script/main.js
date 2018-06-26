e3235b0275868f2b3fabde6d4e5151f2e28c28df
brbrbrbrbr <lumos.colorum@gmail.com>
2018-06-26 16:16:44 +0300

    minor changes

--- script/main.js
+++ script/main.js
@@ -4,15 +4,15 @@ let rowNumber = 3;
 let colNumber = 3;
 let hoverDivOffsetLeft = 0;
 let hoverDivOffsetTop = 0;
+let columnElements = document.getElementsByClassName('col');
 
 let element = document.getElementById('test-content__test-table');
 element.addEventListener( 'mouseover', function () {
-        let buttonVisibility = document.getElementsByClassName('test-content__red-button');
-        for (j = 0; j < 2; j++) {
-            buttonVisibility[j].style.opacity = '1';
-        }
+    let buttonVisibility = document.getElementsByClassName('test-content__red-button');
+    for (j = 0; j < 2; j++) {
+        buttonVisibility[j].style.opacity = '1';
+    }
     element.onmousemove = function (event) {
-        const p = 'px';
         let removeColumn = document.getElementById('test-content__remove-column');
         let removeRow = document.getElementById('test-content__remove-row');
         let target = event.target;
@@ -20,21 +20,41 @@ element.addEventListener( 'mouseover', function () {
         hoverDivOffsetLeft = target.offsetLeft;
         hoverDivOffsetTop = target.offsetTop;
         //console.log(hoverDivOffsetTop + ' ' + hoverDivOffsetLeft);
-        removeColumn.style.marginLeft = hoverDivOffsetLeft + p;
-        removeRow.style.marginTop = hoverDivOffsetTop + p;
+        console.log(`${hoverDivOffsetTop} ${hoverDivOffsetLeft}`);
+        removeColumn.style.marginLeft = `${hoverDivOffsetLeft}px`;
+        removeRow.style.marginTop = `${hoverDivOffsetTop}px`;
+        removeRow.onclick = function(){
+            let squaresCounterRows;
+            squaresCounterRows = Math.trunc(hoverDivOffsetTop / 52);
+            squaresCounterRows++;
+            console.log(squaresCounterRows);
+            for (i = 0; i < colNumber; i++) {
 
+                //columnElements[i].appendChild(addingRow);
+            }
+        };
+        removeColumn.onclick = function(){
+            let squaresCounterColumns;
+            squaresCounterColumns = Math.trunc(hoverDivOffsetLeft / 52);
+            squaresCounterColumns++;
+            console.log(squaresCounterColumns);
+            for(i = 0; i < rowNumber; i++){
+                if(i === squaresCounterColumns){
+                    //for (j = 0; j < colNumber; j++){
+                    columnElements[i].remove();
+                    //}
+                }
+                    }
+        };
     };
 });
 
-
-
-element.addEventListener( 'mouseout', function () {
+/*element.addEventListener( 'mouseout', function () {
     let buttonVisibility = document.getElementsByClassName('test-content__red-button');
     for (j = 0; j < 2; j++) {
         buttonVisibility[j].style.opacity = '0';
     }
-});
-
+});*/
 
 function addColumnToTable(){
     let addEltmentToTable = document.createElement("div");
@@ -47,12 +67,9 @@ function addColumnToTable(){
         colElement.appendChild(addEltmentTocolumn);
     }
     colNumber++;
-    let box = element.getBoundingClientRect();
-
 }
 
 function addRowToTable(){
-    let columnElements = document.getElementsByClassName('col');
     for (i = 0; i < colNumber; i++) {
         let addingRow = document.createElement("div");
         addingRow.className = "test-content__square";
@@ -60,9 +77,7 @@ function addRowToTable(){
     }
     rowNumber++;
 }
-function removeRowFromTable(){
 
-}
 function removeColumnFromTable(){
 
 }
